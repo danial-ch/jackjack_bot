64524af6cc47c992f24d1d678996ae831786b740
AbolfazlArdeshirtanha <ardeshirtanhaa@gmail.com>
2020-07-28 02:07:01 +0430

    randome answering fixed

--- jackjack.py
+++ jackjack.py
@@ -7,14 +7,23 @@ class jack_bot():
         pass
 
     def textHandler(self, bot, update):
-        answers = ["yo", "سلام دیوث", 'سلام']
+        answers = ["yo", "سلام دیوث", 'سلام سکسی', 'چه سلامی چه علیکی!!']
         if update.message.text == "سلام":
-            index = random.randint(0, 2)
+            rand_index = random.randint(0, 100)
+            if 0 < rand_index < 26:
+                index = 0
+            elif 26 < rand_index < 51:
+                index = 1
+            elif 51 < rand_index < 76:
+                index = 2
+            else:
+                index = 3
             str = answers[index]
             bot.send_message(text=str, chat_id=update.message.chat_id)
+       # TODO: add counter for each person and send message if it is bigger than 5
 
     def main(self):
-        token = Updater("1276393434:AAFxJyGaWuj7B2jnc_D7mJ00YG5BFN4AbBw")
+        token = Updater("1276393434:AAFxJyGaWuj7B2jnc_D7mJ00YG5BFN4AbBw", use_context=True)
         token.dispatcher.add_handler(MessageHandler(Filters.text, self.textHandler))
 
         token.start_polling()
